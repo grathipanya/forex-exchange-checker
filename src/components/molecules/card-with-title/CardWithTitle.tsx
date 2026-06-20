1f6c9943ff060af2172f05159f811a3b2a3202fd
grathipanya <165570597+grathipanya@users.noreply.github.com>
2026-06-21 01:15:09 +1000

    wip: graph card, to wire in data and charting library

--- src/components/molecules/card-with-title/CardWithTitle.tsx
+++ src/components/molecules/card-with-title/CardWithTitle.tsx
@@ -5,7 +5,10 @@ import type { PropsWithChildren } from "react";
 export type CardWithTitleProps = PropsWithChildren<{
   title: string;
   titleLocation?: "inside" | "outside";
+  info?: string;
   className?: string;
+  titleClassName?: string;
+  infoClassName?: string;
   footer?: React.ReactNode;
 }>;
 
@@ -13,17 +16,37 @@ const CardWithTitle = ({
   title,
   titleLocation = "outside",
   className = "",
+  titleClassName = "",
+  info,
+  infoClassName = "",
   children,
   footer,
 }: CardWithTitleProps) => {
   return (
     <Card className={cn(`flex flex-col gap-4`, className)}>
       {titleLocation === "outside" && (
-        <div className="text-preset-4 text-neutral-50 uppercase">{title}</div>
+        <div
+          className={cn(
+            "text-preset-4 text-neutral-50 uppercase",
+            titleClassName,
+          )}
+        >
+          {title}
+        </div>
       )}
       <Card className={cn("bg-neutral-700 p-5 gap-5 flex flex-col", className)}>
         {titleLocation === "inside" && (
-          <div className="text-preset-4 text-neutral-50 uppercase">{title}</div>
+          <div className="flex justify-between">
+            <div
+              className={cn(
+                "text-preset-4 text-neutral-50 uppercase",
+                titleClassName,
+              )}
+            >
+              {title}
+            </div>
+            <div className={cn("flex items-center", infoClassName)}>{info}</div>
+          </div>
         )}
         {children}
         {footer && (
--- src/components/organisms/history-chart/HistoryChart.tsx
+++ src/components/organisms/history-chart/HistoryChart.tsx
@@ -5,8 +5,10 @@
 
 import { Card } from "@/components/atoms/card";
 import { Label } from "@/components/atoms/label/";
+import { CardWithTitle } from "@/components/molecules/card-with-title";
 import { cn } from "@/utils/cn";
 import { getDifferenceIcon, isValuePositive } from "@/utils/number";
+import { format } from "date-fns/format";
 import { useState } from "react";
 
 export type CardProps = {
@@ -62,48 +64,82 @@ const HARDCDODED_CARDS_DATA: CardProps = [
 
 const HARDCODED_RANGE_OPTIONS = ["1D", "1W", "1M", "3M", "6M", "1Y"];
 
+const HARDCODED_GRAPH_DATA: GraphData = {
+  currencyPair: { base: "USD", quote: "EUR" },
+  currentDateTime: "2024-06-01T12:00:00Z",
+  exchangeRate: 1.2345,
+  data: [
+    { date: "2024-05-01", value: 1.2 },
+    { date: "2024-05-08", value: 1.21 },
+    { date: "2024-05-15", value: 1.22 },
+    { date: "2024-05-22", value: 1.23 },
+    { date: "2024-05-29", value: 1.2345 },
+  ],
+};
+
 const HistoryChart = () => {
   const [selectedRange, setSelectedRange] = useState("1M");
+
+  const formatGraphInfoHeader = ({ exchangeRate }: GraphData) => {
+    const date = new Date().toLocaleString("en-US", { timeZoneName: "short" });
+
+    const formattedDate = format(date, "MMM dd HH:mm z").toUpperCase();
+    const trimmedDate = formattedDate.slice(0, formattedDate.indexOf("+"));
+
+    return `${exchangeRate} · ${trimmedDate}`;
+  };
+
   return (
-    <div className="flex justify-between items-center pt-5">
-      <div className="grid grid-cols-4 gap-4">
-        {HARDCDODED_CARDS_DATA.map((card) => (
-          <Card
-            key={card.title}
-            className="flex flex-col bg-neutral-700 border border-neuytral-600 gap-4 py-3 px-5 flex-1 min-w-0"
-          >
-            <Label className="text-preset-4 text-neutral-50">
-              {card.title}
-            </Label>
-            <Label
+    <div className="flex flex-col gap-5">
+      <div className="flex justify-between items-center pt-5">
+        <div className="grid grid-cols-4 gap-4">
+          {HARDCDODED_CARDS_DATA.map((card) => (
+            <CardWithTitle key={card.title} titleLocation="inside" {...card}>
+              <Label
+                className={cn(
+                  "text-preset-2 text-neutral-50",
+                  card.colored &&
+                    (isValuePositive(card.value)
+                      ? "text-green-500"
+                      : "text-red-500"),
+                )}
+              >
+                {card.showSymbol && getDifferenceIcon(card.value)} {card.value}
+              </Label>
+            </CardWithTitle>
+          ))}
+        </div>
+        <div className="flex flex-row bg-neutral-700 rounded-8 py-0.5 w-max h-fit">
+          {HARDCODED_RANGE_OPTIONS.map((option) => (
+            <Card
+              key={option}
               className={cn(
-                "text-preset-2 text-neutral-50",
-                card.colored &&
-                  (isValuePositive(card.value)
-                    ? "text-green-500"
-                    : "text-red-500"),
+                "px-4 py-3 text-preset-5 hover:cursor-pointer",
+                selectedRange === option
+                  ? "bg-neutral-500 text-neutral-50"
+                  : "bg-transparent text-neutral-200",
               )}
+              onClick={() => setSelectedRange(option)}
             >
-              {card.showSymbol && getDifferenceIcon(card.value)} {card.value}
-            </Label>
-          </Card>
-        ))}
+              <Label>{option}</Label>
+            </Card>
+          ))}
+        </div>
       </div>
-      <div className="flex flex-row bg-neutral-700 rounded-8 py-0.5 w-max h-fit">
-        {HARDCODED_RANGE_OPTIONS.map((option) => (
-          <Card
-            key={option}
-            className={cn(
-              "px-4 py-3 text-preset-5 hover:cursor-pointer",
-              selectedRange === option
-                ? "bg-neutral-500 text-neutral-50"
-                : "bg-transparent text-neutral-200",
-            )}
-            onClick={() => setSelectedRange(option)}
-          >
-            <Label>{option}</Label>
-          </Card>
-        ))}
+      <div>
+        <CardWithTitle
+          titleLocation="inside"
+          title={
+            HARDCODED_GRAPH_DATA.currencyPair.base +
+            "/" +
+            HARDCODED_GRAPH_DATA.currencyPair.quote
+          }
+          titleClassName="text-preset-3-medium text-neutral-50"
+          info={formatGraphInfoHeader(HARDCODED_GRAPH_DATA)}
+          infoClassName="text-preset-5 text-neutral-50"
+        >
+          big boy graph here
+        </CardWithTitle>
       </div>
     </div>
   );
